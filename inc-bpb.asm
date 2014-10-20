59fd22ad623610ef63f0246828dd282a9cebe17b
Philip Stears <philip@driveworks.co.uk>
2014-10-20 07:56:12 +0100

    Finished structures for ebpb 12/16 and 32

--- inc-bpb.asm
+++ inc-bpb.asm
@@ -24,10 +24,33 @@ endstruc
 
 struc ebpb_fat1216
     .bpb                resb    bpb_size
+	.driveNumber		resb	1
+	.flags				resb	1
+	.signature			resb	1
+	.volumeId			resb	4
+	.volumeLabel		resb	11
+	.systemId			resb	8
+	.bootCode			resb	448
+	.bootSignature		resb	2
 endstruc
 
 struc ebpb_fat32
     .bpb                resb    bpb_size
+	.sectorsPerFat32	resd	1
+	.flags				resw	1
+	.version			resw	1
+	.rootCluster		resd	1
+	.fsInfoSector		resw	1
+	.bootBackupSector	resw	1
+	.reserved			resb	12
+	.driveNumber		resb	1
+	.flags2				resb	1
+	.signature			resb	1
+	.volumeId			resb	4
+	.volumeLabel		resb	11
+	.systemId			resb	8
+	.bootCode			resb	420
+	.bootSignature		resb	2
 endstruc
 
 
